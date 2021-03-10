c2efcf9dd9823a5be4e6bc6b99c735f281628095
Anwesh43 <anweshthecool0@gmail.com>
2021-03-11 00:55:04 +0530

    adding BTCCNode class

--- balltoconccircleview/src/main/java/com/example/balltoconccircleview/BallToConcCircleView.kt
+++ balltoconccircleview/src/main/java/com/example/balltoconccircleview/BallToConcCircleView.kt
@@ -129,4 +129,45 @@ class BallToConcentricCircle(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BTCCNode(var i : Int, val state : State = State()) {
+
+        private var next : BTCCNode? = null
+        private var prev : BTCCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BTCCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBTCCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BTCCNode {
+            var curr : BTCCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
