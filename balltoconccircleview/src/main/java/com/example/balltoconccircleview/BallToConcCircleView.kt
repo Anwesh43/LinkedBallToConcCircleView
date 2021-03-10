9b6de96ffa6d7f09ef9bbe3dad60a8463b8e8f2c
Anwesh43 <anweshthecool0@gmail.com>
2021-03-11 00:49:31 +0530

    adding BallToConcentricCircleView class

--- balltoconccircleview/src/main/java/com/example/balltoconccircleview/BallToConcCircleView.kt
+++ balltoconccircleview/src/main/java/com/example/balltoconccircleview/BallToConcCircleView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawBTCCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBallToConcentricCircle(scale, w, h, paint)
 }
+
+class BallToConcentricCircle(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean{
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
