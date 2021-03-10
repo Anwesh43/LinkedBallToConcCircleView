9ebc34377b2b63762a9b676b55ce611f4f9412bc
Anwesh43 <anweshthecool0@gmail.com>
2021-03-11 01:07:57 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedballtoconccircleview/MainActivity.kt
+++ app/src/main/java/com/example/linkedballtoconccircleview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedballtoconccircleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.balltoconccircleview.BallToConcentricCircleView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BallToConcentricCircleView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
